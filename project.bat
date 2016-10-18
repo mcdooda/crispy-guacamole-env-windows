040fa5db4f2073834730169a3a42178147497718
mcdooda <nicolas.dod@gmail.com>
2016-10-18 22:45:08 +0200

    Added project.bat

---
+++ project.bat
@@ -0,0 +1,9 @@
+rmdir /s /q .\project
+mkdir project\Debug
+mkdir project\Release
+cd project
+cmake .. -G "Visual Studio 14 2015 Win64"
+cd ..
+copy .\dll\* .\project\Debug
+copy .\dll\* .\project\Release
+pause
