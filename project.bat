cd03e8bbee199c77711ea2747082b1d7f6e1b31a
mcdooda <nicolas.dod@gmail.com>
2016-11-03 20:13:19 +0100

    Added RelWithDebInfo folder in project.bat

--- project.bat
+++ project.bat
@@ -1,10 +1,12 @@
 rmdir /s /q .\project
 mkdir project\Debug
 mkdir project\Release
+mkdir project\RelWithDebInfo
 cd project
 cmake .. -G "Visual Studio 14 2015 Win64"
 cd ..
 copy .\dll\* .\project\Debug
 copy .\dll\* .\project\Release
+copy .\dll\* .\project\RelWithDebInfo
 copy .\crispy-guacamole.vcxproj.user .\project
 pause
