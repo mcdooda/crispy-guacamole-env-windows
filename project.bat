5cf7bea1616ca7f514a96a637f04490cc107bdbd
Rouillard <jon@rouillard.org>
2017-03-08 21:19:08 +0100

    Bugfix in bat file

--- project.bat
+++ project.bat
@@ -3,7 +3,7 @@ mkdir project\Debug
 mkdir project\Release
 mkdir project\RelWithDebInfo
 cd project
-cmake ..
+cmake .. -G "Visual Studio 14 2015 Win64"
 cd ..
 copy .\dll\* .\project\Debug
 copy .\dll\* .\project\Release
